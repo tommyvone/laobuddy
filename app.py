a2030e02b6738e0f5370ccaf55f9d5ad654993b6
Vone Bountham <tommybountham@yahoo.com>
2025-09-21 19:52:45 -0400

    modified and add flask route

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 @app.route('/login')
 def login():
-    return render_template('login')
+    return render_template('login.html')
 
-
-
-app.run(debug=True)
-
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
