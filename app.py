94518dac898b3e44b7a62306b990d20657078128
Vone Bountham <tommybountham@yahoo.com>
2025-09-21 20:20:45 -0400

    add and create user table

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
+from Anyfunctions import
 
 app = Flask(__name__)
 
-
+# main route
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/signup')
+
+@app.route('/signup',methods=['GET','POST'])
 def signup():
-    return render_template('signup.html')
 
 @app.route('/login')
 def login():
@@ -19,3 +20,36 @@ def login():
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
+
+
+# Create SQlite table ifit doesn't 
+def init_db():
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXIST users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            email TEXT NOT NULL,
+            password TEXT NOT NULL
+            )
+        ''')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
