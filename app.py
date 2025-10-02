582b169f183eee34e0f52c03a845ec6eccb942c0
Vone Bountham <tommybountham@yahoo.com>
2025-10-02 04:13:27 -0400

    add javascript to validate user input

--- app.py
+++ app.py
@@ -17,22 +17,41 @@ db.init_app(app)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
+
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 
+
 @app.route('/signup', methods=['GET'])
 def show_signup_form():
     return render_template('signup.html')
+    
 
 
 @app.route('/signup', methods=['POST'])
 def signup():
+    
+    username = request.form.get('username')
+    email = request.form.get('email')
+
+    # check if username or email already exists
+    existin_user = Member.query.filter(
+        (Member.username == username) | (Member.email == email)).first()
+    if existin_user:
+        error = "Username or email already exists. Please choose another."
+        return render_template('signup.html',error=error)
+
+    # Proceed with creating the new membeer
     member = Member(
         first_name=request.form.get('firstName'),
         last_name=request.form.get('lastName'),
@@ -43,11 +62,12 @@ def signup():
         country=request.form.get('country'),
         city=request.form.get('city')
     )
-    db.session.add(member)
-    db.session.commit()
-    return render_template('success.html')
+   
+        db.session.add(member)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print("Error:", e)
+        return render_template('signup.html', error="Something went wrong.")
 
-@app.route('/success')
-def success():
-    return render_template('success.html')
 
